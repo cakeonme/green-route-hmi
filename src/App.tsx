50397d5a9cb8cf5d8592c85322e563c3529d8793
Jimin Seo <jiminseo@Jiminui-MacBookAir.local>
2025-09-23 18:07:55 +0900

    feat: project setting

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-green-600">
+        🌱 개발 환경 세팅 성공!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
